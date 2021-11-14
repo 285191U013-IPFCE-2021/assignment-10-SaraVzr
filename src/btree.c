47917c0bf28494c12fec202141d4d5eada3469a6
SaraVzr <sara.vaziri@yahoo.com>
2021-11-14 20:26:25 +0100

    commited

--- src/btree.c
+++ src/btree.c
@@ -5,46 +5,107 @@
 #include<assert.h>
 #include<stdbool.h>
 
+int min_right (struct tree_node *t){
+  struct tree_node *temp = t;
+  while(t->left != NULL){
+   temp = t->left;
+  }
+  return (temp->item);
+}
 
 struct tree_node * Insert (int x, struct tree_node *t)
 {
   // Insert item x into the tree t
-
-  return NULL;
+ if (t==NULL){
+  t = Initialize (t);
+  t->item = x;
+  }
+   else if(x <= t->item)
+   return Insert (x, t->left);
+ 
+     else if (x > t->item)
+     return Insert (x, t->right);
 }
 
 struct tree_node * Remove (int x, struct tree_node *t)
 {
  // Remove one item from the tree t
+ if (t!=NULL)
+ {
+  if(x > t->item){
+    t->right = Remove(x, t->right);
+   } 
+     else if(x < t->item){
+     t->left = Remove (x, t->left);
+     }
+       else if (x == t->item){
+  
+       if ( t->left == NULL && t->right ==NULL) {
+       free (t);
+       return NULL;
+       }
+       }
+         else if (t->left == NULL || t->right == NULL){
+         struct tree_node *p = NULL;
+         if (t->right == NULL){
+          p = t->left;
+         }
+         else { 
+         p = t->right;
+         }
+         free(t);
+         return p; 
+         }
 
-  return NULL;
+           else {
+           int min_largest = min_right (t->right);
+           t->item = min_largest;
+           t->right = Remove(t->item, t->right);
+           }
+ }
+  return (t);
 }
+
 
 
 int Contains (int x, struct tree_node *t)
 {
-
   // Return true if the tree t contains item x. Return false otherwise.
-
-  return NULL;
-
+  if (t->item == x)
+  return 1;
+     else if (x < t->item)
+     Contains(x, t->left);
+       else if (x > t->item)
+       Contains (x,t->right);
+         else 
+         return 0;
 }
 
 
 struct tree_node * Initialize (struct tree_node *t)
 {
   // Create an empty tree
-  return NULL;
+  assert (t==NULL);
+  struct tree_node *binarytree= (struct tree_node*)malloc(sizeof(struct tree_node));  
+  binarytree->left = NULL;
+  binarytree->right = NULL;
+  return binarytree;
 }
+
 
 int Empty (struct tree_node *t)
 {
   // Test if empty
+  if (t == NULL)
+    return 1;
+
   return 0;
 }
 
 int Full (struct tree_node *t)
 {
     // Test if full
-    return 0;
+    return false;
 }
+
+
--- src/linked_list.c
+++ src/linked_list.c
@@ -13,46 +13,68 @@ node *make_node (int v, node * q)
   node *p = malloc (sizeof (node));
   p->value = v;
 
-  if (q == &SENTINEL_node)
-    p->next = &SENTINEL_node;
+  if (q == NULL)
+    p->next = NULL;
   else
     p->next = q;
   return p;
 }
 
 /* free all nodes in the list p */
-void free_list (node * p)
+node* free_list (node * p)
 {
   node *q = p;
-  while (q != &SENTINEL_node)
+  while (q != NULL)
     {
       node *t = q->next;
       free (q);
       q = t;
     }
+    return q;
 }
 
 
 /* print list to console */
 void print_list (node * p)
 {
-    // Add your code for exercise 1
-    // There is NO testcode for this
-}
+   //exercise 1
+     if (p->next !=NULL){
+      printf("%d", p->value);
+      print_list (p->next);
+     }  
+     else
+        printf ("%d", p->value); //print the value in the node 
+ }
 
 int sum_squares (node * p)
 {
-    // Add your code for excercise 2
-    // You can find the tests in tests.cpp
-    return -1;
+    //excercise 2
+    if (p->next != NULL) //if we have an empty list 
+       return (square (p->value) + sum_squares (p->next));
+    else
+       return (square (p->value));
 }
+
 
 node *map (node * p, int (*f) (int))
 {
-    // Add your code for excercise 3
-    return NULL;
-}
+    //excercise 3
+    int (*sf)(int);
+    sf = square;
+    int x;
+    x = sf(p->value);
+    node* sq_list = make_node (x, NULL);
 
+    if (p->next !=NULL)
+    {
+      sq_list->next = map (p->next, square);
+      return sq_list;
+    } 
+    else
+    {
+      return 0;
+    }
+}
 
 int square (int x)
 {
